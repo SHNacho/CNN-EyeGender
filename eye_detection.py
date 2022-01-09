0c95542d98b62c640247ad34b6a0112ac45cddc0
Nacho Sánchez <nachosanchezh@hotmail.com>
2022-01-09 15:16:53 +0000

    first commit

--- eye_detection.py
+++ eye_detection.py
@@ -18,15 +18,17 @@ while True:
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.25, 5)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            ojo = roi_color[ey:ey+eh, ex:ex+ew]
+    eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+    for(x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x+w+10, y+h+10), (0, 255, 0), 2)
+        ojo = frame[y:y+h, x:x+w]
+
+        # roi_gray = gray[y:y+h, x:x+w]
+        # roi_color = frame[y:y+h, x:x+w]
+        # eyes = eye_cascade.detectMultiScale(roi_gray, 1.25, 5)
+        # for (ex, ey, ew, eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex, ey), (ex + ew + 10, ey + eh + 10), (0, 255, 0), 2)
+        #     ojo = roi_color[ey:ey+eh, ex:ex+ew]
         
     cv2.imshow('frame', frame)
 
@@ -34,10 +36,10 @@ while True:
         break
 
 while cv2.waitKey(1) != ord('w'):
-    cv2.imshow('ojo', ojo)
+    cv2.imshow('ojo', ojo[2:-2 , 2:-2])
 
 print(ojo)
-ojo = cv2.resize(ojo, (75, 75), interpolation = cv2.INTER_AREA)
+ojo = cv2.resize(ojo[2:-2 , 2:-2], (75, 75), interpolation = cv2.INTER_AREA)
 ojo_tensor = image.img_to_array(ojo)
 ojo_tensor = np.expand_dims(ojo, axis=0)
 pred = model.predict(ojo_tensor)
